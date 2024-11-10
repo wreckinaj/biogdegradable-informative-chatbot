fc13cfe8c75f4ee53f6a9399c147370cf0a3bfb6
baoph <baokhanh0514@gmail.com>
2024-11-09 22:08:02 -0700

    Implemented pressing enter to send message

--- frontend_template/src/components/ChatInput.tsx
+++ frontend_template/src/components/ChatInput.tsx
@@ -15,9 +15,14 @@ const ChatInput: React.FC<ChatinputProps> = ({onSend}) => {
         }
     };
 
+    const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+        if (event.key == 'Enter')
+            handleSend();
+    };
+
     return (
         <div className="chat-input">
-            <input type="text" value = {input} onChange={ (e) => setInput(e.target.value)} placeholder="Type your message" className="Prompt_box"/>
+            <input type="text" value = {input} onKeyDown={handleKeyPress} onChange={ (e) => setInput(e.target.value)} placeholder="Type your message" className="Prompt_box"/>
             <button onClick={handleSend} className="Prompt_send">Send</button>
         </div>
     );
